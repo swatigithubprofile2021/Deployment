de69291fc1eb13358eff1ddbef6436e5e024a3c0
swatigithubprofile2021 <86872111+swatigithubprofile2021@users.noreply.github.com>
2022-08-17 19:49:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,url_for
 import pickle
 import numpy as np
 
@@ -8,17 +8,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def man():
+    pred=''
     return render_template('home.html')
 
-@app.route('/predict',methods = ['post'])
+@app.route('/predict',methods = ['POST'])
 def home():
+
     data1 = request.form['a']
     data2 = request.form['b']
     data3 = request.form['c']
     data4 = request.form['d']
-    arr = np.array[[data1,data2,data3,data4]]
-    pred = model.predict(arr)
-    return render_template('after.html',data = pred)
+    #arr = np.array[[data1,data2,data3,data4]]
+    pred = model.predict([[data1,data2,data3,data4]])
+    return render_template('after.html', data=pred)
 
 if __name__ == "__main__":
     app.run(debug = True)
