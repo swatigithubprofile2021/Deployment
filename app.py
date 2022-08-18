c67097dd192b9ee053d8037f4061285636a05092
swatigithubprofile2021 <86872111+swatigithubprofile2021@users.noreply.github.com>
2022-08-18 18:52:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def man():
     pred=''
-    return render_template('Templates/home.html')
+    return render_template('templates/home.html')
 
 @app.route('/predict',methods = ['POST'])
 def home():
@@ -20,7 +20,7 @@ def home():
     data4 = request.form['d']
     #arr = np.array[[data1,data2,data3,data4]]
     pred = model.predict([[data1,data2,data3,data4]])
-    return render_template('Templates/after.html', data=pred)
+    return render_template('templates/after.html', data=pred)
 
 if __name__ == "__main__":
     app.run(debug = True)
